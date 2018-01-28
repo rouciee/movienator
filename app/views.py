1d9d7df9d4b295baa1348cb8c246a1d18fc3be51
Bryan Collazo <bcollazo2010@gmail.com>
2018-01-28 17:03:21 -0400

    Yield random movie

--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+import random
 
+from django.shortcuts import render
 from app.models import Movie
 
+
 def index(request):
-	print(Movie.objects.count())
-	return render(request, 'index.html', {'year': '2001', 'rating': '8.3', 'hours': '2', 'minutes': '32', 'genres': 'Adventure, Family, Fantasy'})
+    movies = Movie.objects.filter(is_adult=False, released_year__gt=2015,
+                                  imdb_rating__gt=7, imdb_num_votes__gt=10000)
+
+    count = movies.count()
+    i = random.randint(0, count - 1)
+    movie = movies.all()[i]
+    print(movie)
+
+    return render(request, 'index.html', {
+        'year': movie.released_year,
+        'rating': movie.imdb_rating, 'hours': '2', 'minutes': '32',
+        'genres': 'Adventure, Family, Fantasy'})
