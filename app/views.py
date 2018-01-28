11615bfb45d0644d8ac57abb5733ecf2ce63cb75
Rocio <ro.garciair@gmail.com>
2018-01-28 16:58:17 -0400

    Convert to template

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from app.models import Movie
 
 def index(request):
-	return render(request, 'index.html', {'name': 'Bryan'})
+	print(Movie.objects.count())
+	return render(request, 'index.html', {'year': '2001', 'rating': '8.3', 'hours': '2', 'minutes': '32', 'genres': 'Adventure, Family, Fantasy'})
