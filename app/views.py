b2560e3c8924a35331a38224f492b327b9025e88
Bryan Collazo <bcollazo2010@gmail.com>
2018-02-05 18:28:29 -0400

    Hours and minutes

--- app/views.py
+++ app/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 from app.models import Movie
 
 
+def hours_and_minutes(runtime):
+    return (int(runtime / 60), runtime % 60)
+
+
 def index(request):
     movies = Movie.objects.filter(
         is_adult=False, imdb_rating__gt=5, imdb_num_votes__gt=400, released_year__gt=2000)\
@@ -11,14 +15,16 @@ def index(request):
         .exclude(youtube_trailer_key="-1")\
         .exclude(poster_path=None)
     count = movies.count()
-    print(count)
+
     if count == 0:
         movie = Movie.objects.first()
     else:
         i = random.randint(0, count - 1)
         movie = movies.all()[i]
     genre_string = ', '.join([g.name for g in movie.genres.all()])
+    (hours, minutes) = hours_and_minutes(movie.runtime)
 
+    print(count)
     print(movie)
 
     return render(request, 'index.html', {
@@ -26,7 +32,7 @@ def index(request):
         'overview': movie.overview,
         'year': movie.released_year,
         'rating': movie.imdb_rating,
-        'hours': '2', 'minutes': '32',
+        'hours': hours, 'minutes': minutes,
         'poster_path': movie.poster_path,
         'youtube_trailer_key': movie.youtube_trailer_key,
         'genres': genre_string
