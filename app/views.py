a483d6d8cb69d8ed5b596e0948da29bee4bc7219
Rocio <ro.garciair@gmail.com>
2018-02-05 18:15:34 -0400

    Cambio num of votes and rating

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from app.models import Movie
 
 def index(request):
     movies = Movie.objects.filter(
-        is_adult=False, imdb_rating__gt=6, imdb_num_votes__gt=5000, released_year__gt=2000)\
+        is_adult=False, imdb_rating__gt=5, imdb_num_votes__gt=400, released_year__gt=2000)\
         .exclude(youtube_trailer_key=None)\
         .exclude(youtube_trailer_key="-1")\
         .exclude(poster_path=None)
